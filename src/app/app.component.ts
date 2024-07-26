b154b85b08279e3ef20edc63c1f464964a8aca86
MAXII\maxii <maximaxmaxii690@gmail.com>
2024-07-26 20:24:00 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'despensa';
+              // "node_modules/bootstrap/dist/js/bootstrap.min.js",
+              // "node_modules/@popperjs/core/dist/umd/popper.min.js"
+
 }
